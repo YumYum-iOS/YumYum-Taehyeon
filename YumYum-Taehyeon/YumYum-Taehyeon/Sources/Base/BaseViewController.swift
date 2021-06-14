599ddd8a05b82cd2195015f2513f79bd24f11fe8
김태현 <taehyeon.dev@gamil.com>
2021-06-14 14:48:25 +0900

    add: 공통 요소 담은 BaseViewController 생성
    - 앞으로 만들 뷰 컨트롤러는 해당 뷰컨을 상속받아 생성
    - ex. navigation bar 숨김

---
+++ YumYum-Taehyeon/YumYum-Taehyeon/Sources/Base/BaseViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BaseViewController.swift
+//  YumYum-Taehyeon
+//
+//  Created by taehy.k on 2021/06/14.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationController?.navigationBar.isHidden = true
+    }
+}
