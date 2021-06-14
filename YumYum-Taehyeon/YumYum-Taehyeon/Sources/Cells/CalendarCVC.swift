20e269f6a81e7cfbcd1d537e99b68a95b72bc949
김태현 <taehyeon.dev@gamil.com>
2021-06-14 18:59:59 +0900

    add: 캘린더 뷰 셀 생성
    - emptyView
    - dataLabel

---
+++ YumYum-Taehyeon/YumYum-Taehyeon/Sources/Cells/CalendarCVC.swift
@@ -0,0 +1,21 @@
+//
+//  CalendarCVC.swift
+//  YumYum-Taehyeon
+//
+//  Created by taehy.k on 2021/06/14.
+//
+
+import UIKit
+
+class CalendarCVC: UICollectionViewCell {
+    
+    static let identifier = "CalendarCVC"
+    
+    @IBOutlet weak var dataLabel: UILabel!
+    @IBOutlet weak var emptyView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
