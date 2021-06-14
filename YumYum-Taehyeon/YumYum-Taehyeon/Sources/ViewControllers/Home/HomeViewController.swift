ca797240e0390dab3f9b6468085940bea25155c5
김태현 <taehyeon.dev@gamil.com>
2021-06-14 18:57:26 +0900

    add: HomeViewController 뼈대
    - 홈 뷰 컨트롤러 뼈대 생성 (아직 아무것도 없음)

---
+++ YumYum-Taehyeon/YumYum-Taehyeon/Sources/ViewControllers/Home/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  YumYum-Taehyeon
+//
+//  Created by taehy.k on 2021/06/14.
+//
+
+import UIKit
+
+class HomeViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .blue
+    }
+}
